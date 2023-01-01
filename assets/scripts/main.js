102b0831c72d37d3cd235989f0b1195c31f10fe1
Swirek3331 <karolsutkowski21@gmail.com>
2023-01-01 12:45:36 +0100

    Ło jezu, testy
    Kurwa, jakie to męczoce

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,6 +6,7 @@ class Server {
     owner = null;
     joinDate;
     invateLink = null;
+    img; //Link do obrazu
 
     //Booleans
     publicInvateLink = false; //czy zaprosznie ma być na stronie
@@ -13,6 +14,9 @@ class Server {
 
     //Numbers
     mebmers = totalMebers - bots; //liczba ludzi na serwerze
+
+    //Long strings
+    description; //opis serwera
 
     constructor(name) {
         this.name = name;
@@ -24,10 +28,13 @@ const plac = new Server("Plac", {
     owner: "[PL][KT][ZPGM]Swirek3331#0611",
     joinDate: "Always have been",
     invateLink: "https://discord.gg/A9rNQdJgZZ",
+    img: "https://swirek3331.github.io/Plac/assets/images/icons/servers/plac.png",
     
     publicInvateLink: true,
     publicOwner: true,
 
     totalMebers: 4,
     bots: 1,
+
+    description: "Serwer do którego strona się odnosi."
 });
