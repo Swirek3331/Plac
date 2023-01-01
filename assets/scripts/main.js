5cd3570fb701f2563fb63baeb441129e53be1aa8
Swirek3331 <karolsutkowski21@gmail.com>
2023-01-01 12:12:09 +0100

    Klasa

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,22 @@
 //Pytanie czy to jQuerry tu zadziała.
+
+class Server {
+
+    //Strings
+    name;
+    id;
+    owner = null;
+    joinDate;
+    invateLink = null;
+
+    //Booleans
+    publicInvateLink = false; //czy zaprosznie ma być na stronie
+    publicOwner = false; //czy właściciel ma być na stronie
+
+    //Numbers
+    mebmers; 
+
+    constructor(name) {
+        this.name = name;
+    }
+}
