faaf703296f6b0c175d7643439086daee76d0399
Swirek3331 <karolsutkowski21@gmail.com>
2023-01-01 12:21:54 +0100

    Objekt

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,8 @@
-//Pytanie czy to jQuerry tu zadziała.
-
 class Server {
 
     //Strings
     name;
-    id;
+    id; //nie wiem po co, ale jest.
     owner = null;
     joinDate;
     invateLink = null;
@@ -14,9 +12,22 @@ class Server {
     publicOwner = false; //czy właściciel ma być na stronie
 
     //Numbers
-    mebmers; 
+    mebmers = totalMebers - bots; //liczba ludzi na serwerze
 
     constructor(name) {
         this.name = name;
     }
 }
+
+const plac = new Server("Plac", {
+    id: "1033027380870123660",
+    owner: "[PL][KT][ZPGM]Swirek3331#0611",
+    joinDate: "Always have been",
+    invateLink: "https://discord.gg/A9rNQdJgZZ",
+    
+    publicInvateLink: true,
+    publicOwner: true,
+
+    totalMebers: 4,
+    bots: 1,
+});
